b0759c88849df737e3abf13cc72d50f8a0610782
User <User@DESKTOP-K850N6G>
2023-05-16 17:00:42 -0300

    Api Spring

---
+++ src/main/java/com/ldsystems/api/SpringApi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.ldsystems.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author User
+ */
+@SpringBootApplication
+public class SpringApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApi.class, args);
+    }
+}
